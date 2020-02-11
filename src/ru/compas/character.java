7b67d6872f727a5e91b348726be8a07c6575e0ef
XinaT <shelubskiimisha@gamil.com>
2020-02-11 17:21:20 +0300

    drafts

---
+++ src/ru/compas/character.java
@@ -0,0 +1,35 @@
+package ru.compas;
+
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+
+public class character extends JLabel {
+
+    int hp;
+    int mana;
+    String name;
+    String abilities;
+    ArrayList<player> battleUnits;
+
+    int velocity = 10;
+
+
+    public  void move(String direction) {
+        int x = this.getX();
+        int y = this.getY();
+        if (direction.equals("right")) {
+            this.setLocation(x + velocity, y);
+        }
+        if (direction.equals("left")) {
+            this.setLocation(x - velocity, y);
+        }
+        if (direction.equals("forward")){
+            this.setLocation(x,y - velocity);
+        }
+        if (direction.equals("toward")){
+            this.setLocation(x,y + velocity);
+        }
+
+    }
+}
---
+++ src/ru/compas/player.java
@@ -0,0 +1,18 @@
+package ru.compas;
+
+import ru.compas.character;
+
+import java.util.ArrayList;
+
+public class player extends character {
+    int strength;
+    int intellect;
+    int damage;
+    int level;
+    int xp;
+    int protection;
+    String type;
+
+    boolean battleAction;
+    boolean dialogue;
+}
