aa946db58f0fc9d245097a110e5f89a7aecac9e3
Tapok-1 <ilya.Kuzmin00@list.ru>
2020-02-11 17:59:08 +0300

    added IP

---
+++ src/IP.java
@@ -0,0 +1,9 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class IP {
+    public static void main(String[] args) throws UnknownHostException {
+        System.out.println(InetAddress.getLocalHost().getHostAddress());
+    }
+
+}
