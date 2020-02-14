32524d23cfff7e29393b93182a667810eb3635bc
compus <sdfs@sd.ru>
2020-02-14 17:31:50 +0300

    ball

---
+++ src/com/company/Main1.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main1 {
+    public static void main(String[] args) {
+        int i = 1;
+        for (i = 1; i < 11; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
