7226b8ed05814e4bb72af36f268428687998dd6c
compus <sdfs@sd.ru>
2020-02-14 18:03:29 +0300

    ball

---
+++ src/ru/compas/draft/Dich.java
@@ -0,0 +1,25 @@
+package ru.compas.draft;
+
+public class Dich {
+    public static void main(String[] args) {
+        int a = 14;
+        int b = 12;
+        int c = 3;
+
+        age();
+
+
+    }
+
+    static int sum(int a1, int b1, int c1) {
+        int result = a1 + b1 + c1;
+        return result;
+
+    }
+
+    static void age() {
+        System.out.println(14);
+
+    }
+
+}
