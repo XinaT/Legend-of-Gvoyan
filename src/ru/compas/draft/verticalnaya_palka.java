1f6da9bd070479e4358669dc68edb79e08966e67
compus <sdfs@sd.ru>
2020-02-21 18:28:29 +0300

    ball

--- src/ru/compas/draft/verticalnaya_palka.java
+++ src/ru/compas/draft/verticalnaya_palka.java
@@ -8,10 +8,36 @@ public class verticalnaya_palka {
 
     }
 
-    static boolean isPointOnPalka(Point point, Palka palka) {
-        if(palka.a.x == palka.b.y && palka.a.y < point.y || point.y < palka.a.x) {
+    static boolean isPointOnVerticalPalka(Point point, Palka palka) {
+        if(palka.a.x == palka.b.x && (palka.a.y < point.y || point.y < palka.a.y)) {
             return true;
-        } else if(palka.b.x == palka.a.y && palka.b.y < point.y || point.y < palka.b.x){
+        } else if(palka.b.x == palka.a.x && (palka.b.y < point.y || point.y < palka.a.y)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    static boolean isPointOnHorizontalPalka(Point point, Palka palka) {
+        if(palka.a.y == palka.b.y && (palka.a.x < point.x || point.x < palka.a.x)) {
+            return true;
+        } else if(palka.b.y == palka.a.y && (palka.b.x < point.x || point.x < palka.b.x)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    static boolean isPalkaVertical(Palka palka) {
+        if(palka.a.x == palka.b.x) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    static boolean isPalkaOnHorizontal(Palka palka) {
+        if(palka.a.y == palka.b.y) {
             return true;
         } else {
             return false;
