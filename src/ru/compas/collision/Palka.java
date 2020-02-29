3b9704c661390f98869fc056db1df1afc4d712de
vm_razumovsky <vm.razumovsky@gmail.com>
2020-02-29 19:52:24 +0300

    добавлена логика CollisionUtils для определения пересечения персонажа и отрезков

---
+++ src/ru/compas/collision/Palka.java
@@ -0,0 +1,18 @@
+package ru.compas.collision;
+
+
+public class Palka {
+    Point a;
+    Point b;
+    boolean isVertical;
+    boolean isHorizontal;
+
+    Palka(Point a, Point b) {
+        this.a = a;
+        this.b = b;
+
+        // Палка вертикальная, если координаты x точек a и b овпадают
+        isVertical = a.x == b.x;
+        isHorizontal = a.y == b.y;
+    }
+}
---
+++ src/ru/compas/collision/Point.java
@@ -0,0 +1,11 @@
+package ru.compas.collision;
+
+public class Point {
+    int x;
+    int y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
