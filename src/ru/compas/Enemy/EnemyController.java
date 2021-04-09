083609db753bebba92b277d162bf9be4c06aa79e
Nikolai656 <82043959+Nikolai656@users.noreply.github.com>
2021-04-09 20:19:35 +0300

    Enemy distanceBetween

--- src/ru/compas/Enemy/EnemyController.java
+++ src/ru/compas/Enemy/EnemyController.java
@@ -1,13 +1,17 @@
 package ru.compas.Enemy;
 
+import ru.compas.Combo_General;
 import ru.compas.MapLocation;
 import ru.compas.collision.CollisionObject;
 import ru.compas.collision.CollisionUtils;
 import ru.compas.collision.Palka;
+import ru.compas.player;
+import ru.compas.utils.Utils;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 
 public class EnemyController {
     public static void createEnemies(MapLocation map) {
@@ -15,15 +19,38 @@ public class EnemyController {
         Enemy enemy1 = new Enemy(1500, 2200);
         map.add(enemy1);
         setEnemyMovement(enemy1, map);
+        setEnemyVision(enemy1, map);
 
         Enemy enemy2 = new Enemy(1700, 2200);
         map.add(enemy2);
         setEnemyMovement(enemy2, map);
+        setEnemyVision(enemy2, map);
 
         Enemy enemy3 = new Enemy(1800, 2200);
         map.add(enemy3);
         setEnemyMovement(enemy3, map);
+        setEnemyVision(enemy3, map);
     }
+
+
+    static void setEnemyVision(Enemy enemy, MapLocation map) {
+        Timer timer = new Timer(3000, null);
+        timer.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                player player = Combo_General.list_players.get(0);
+                double distanceBetween = Utils.distanceBetween(player, enemy, map, true);
+                if (distanceBetween < 300) {
+                    System.out.println("Ахтунг");
+                }
+            }
+        });
+        timer.start();
+    }
+
+
+
+
     static void setEnemyMovement(Enemy enemy, MapLocation map) {
         Timer timer = new Timer(30,null);
         timer.addActionListener(new ActionListener() {
