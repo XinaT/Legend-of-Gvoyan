53014aa1cedcca258830844ec51c42bd722f6a09
compus <sdfs@sd.ru>
2021-04-09 20:22:32 +0300

    VolosatuePalki builded

---
+++ src/ru/compas/objects/VolosatayaPalkaController.java
@@ -0,0 +1,47 @@
+package ru.compas.objects;
+
+import ru.compas.MapLocation;
+
+public class VolosatayaPalkaController {
+    public VolosatayaPalkaController(MapLocation map) {
+
+    }
+
+    public static void createVolosatayaPalka(MapLocation map) {
+
+        VolosatayaPalka volosatayaPalka = new VolosatayaPalka(803, 2713, 130, 130);
+        map.add(volosatayaPalka);
+
+        VolosatayaPalka volosatayaPalka2 = new VolosatayaPalka(574, 2270, 130, 130);
+        map.add(volosatayaPalka2);
+
+        VolosatayaPalka volosatayaPalka3 = new VolosatayaPalka(653,2269, 130, 130);
+        map.add(volosatayaPalka3);
+
+        VolosatayaPalka volosatayaPalka4 = new VolosatayaPalka(719,2266, 130, 130);
+        map.add(volosatayaPalka4);
+
+        VolosatayaPalka volosatayaPalka5 = new VolosatayaPalka(552,2385, 130, 130);
+        map.add(volosatayaPalka5);
+
+        VolosatayaPalka volosatayaPalka6 = new VolosatayaPalka(646,2388, 130, 130);
+        map.add(volosatayaPalka6);
+
+        VolosatayaPalka volosatayaPalka7 = new VolosatayaPalka(1251,2776, 130, 130);
+        map.add(volosatayaPalka7);
+
+        VolosatayaPalka volosatayaPalka8 = new VolosatayaPalka(1240,2602, 130, 130);
+        map.add(volosatayaPalka8);
+
+        VolosatayaPalka volosatayaPalka9 = new VolosatayaPalka(678,2066, 130, 130);
+        map.add(volosatayaPalka9);
+
+        VolosatayaPalka volosatayaPalka10 = new VolosatayaPalka(750,2171, 130, 130);
+        map.add(volosatayaPalka10);
+
+        VolosatayaPalka volosatayaPalka11 = new VolosatayaPalka(613,1997, 130, 130);
+        map.add(volosatayaPalka11);
+        VolosatayaPalka volosatayaPalka12 = new VolosatayaPalka(1406,2752, 130, 130);
+        map.add(volosatayaPalka12);
+    }
+}
