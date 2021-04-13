481ef3627c9afa9d9816b2d331288bdc210454aa
Liza <Liza@example.com>
2021-04-13 20:24:03 +0300

    Merge branch 'enemy' of https://github.com/XinaT/Legend-of-Gvoyan into master3
    # Conflicts:
    #	src/ru/compas/controller.java

--- src/ru/compas/Main_GENERAL.java
+++ src/ru/compas/Main_GENERAL.java
@@ -1,42 +1,30 @@
 package ru.compas;
 
-import ru.compas.Enemy.EnemyController;
 import ru.compas.network.Client;
-import ru.compas.objects.VolosatayaPalkaController;
 
 import javax.swing.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class Main_GENERAL {
-    public static Pers player;
     static JFrame frame;
     public static String ip_server = "";
     static int server_port = 1800;
+
     public static void main(String[] args) throws IOException {
 
-            Combo_General.make_start(false);
-            frame = Combo_General.creatOkno();
-            Combo_General.pane = Combo_General.pane_made(frame);
+        Combo_General.make_start(false);
 
-
-            MapLocation map = Combo_General.creatMap(-2000,-2000,("Правая нижняя часть карты 2.png"),frame);
-            ArrayList<MapLocation> maps = new ArrayList<>();
-            maps.add(map);
-
-            Thread thread = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        Client.Client_network_make(ip_server, server_port);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Client.Client_network_make(ip_server, server_port);
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            });
-            thread.start();
-        }
-
+            }
+        });
+        thread.start();
     }
+
+}
--- src/ru/compas/network/Main_GENERAL_Server.java
+++ src/ru/compas/network/Main_GENERAL_Server.java
@@ -1,37 +1,15 @@
 package ru.compas.network;
 
 import ru.compas.Combo_General;
-import ru.compas.MapLocation;
-import ru.compas.controller;
-import ru.compas.Pers;
 
-import javax.swing.*;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class Main_GENERAL_Server {
 
     public static int port_of_server = 1800;
-    static JFrame frame;
     public static void main(String[] args) {
 
-        frame = Combo_General.creatOkno();
-        Combo_General.pane = Combo_General.pane_made(frame);
-        Combo_General.isServer = true;
         Combo_General.make_start(true);
-
-        ArrayList<MapLocation> maps = new ArrayList<>();
-        MapLocation map = Combo_General.creatMap(-2000,-2000,("Правая нижняя часть карты 2.png"),frame);
-        maps.add(map);
-        Combo_General.maps = maps;
-
-        Combo_General.list_players = new ArrayList<>();
-        Pers player = Combo_General.player_make(new ImageIcon("pers.png"), 300, 300, "I", -2000, -2000);
-        Combo_General.list_players.add(player);
-
-        frame.setVisible(true);
-
-        controller controller = new controller(frame, player, maps);
 
 
         Thread thread = new Thread(new Runnable() {
