eb92042acb1fea412fe364001d669c0afbb39323
MrSeev <Sladkiypersik4^@gmail.com>
2021-04-13 19:48:17 +0300

    Почистил проект.

--- src/ru/compas/Main_GENERAL.java
+++ src/ru/compas/Main_GENERAL.java
@@ -23,31 +23,7 @@ public class Main_GENERAL {
         public static void main(String[] args) throws IOException {
             Combo_General.make_start(false);
 
-//            frame = Combo_General.creatOkno();
-//            Combo_General.pane = Combo_General.pane_made(frame);
-//            Combo_General.isServer = false;
 
-
-//        MapLocation map = Combo_General.creatMap(-2000, -2000, ("Правая нижняя часть карты 2.png"), frame);
-//        ArrayList<MapLocation> maps = new ArrayList<>();
-//        maps.add(map);
-
-
-//        Combo_General.list_players = new ArrayList<>();
-//        Pers player = Combo_General.player_make(new ImageIcon("pers.png"), 300, 300, "I", -2000, -2000);
-//        Combo_General.list_players.add(player);
-//        Combo_General.create_backpack(frame);
-
-//        Combo_General.maps = maps;
-
-
-            // создание контроллера персонажа, который отвечает за перемещение и за коллизии
-//            controller controller = new controller(frame, player, maps);
-
-//            EnemyController.createEnemies(map);
-
-
-//            frame.setVisible(true);
             Thread thread = new Thread(new Runnable() {
                 @Override
                 public void run() {
