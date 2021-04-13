5b174e8351d50cbb42fca64795348984cd984f0c
MrSeev <Sladkiypersik4^@gmail.com>
2021-04-13 19:05:30 +0300

    Сокращены файлы main

--- src/ru/compas/network/Main_GENERAL_Server.java
+++ src/ru/compas/network/Main_GENERAL_Server.java
@@ -11,29 +11,31 @@ import java.util.ArrayList;
 
 public class Main_GENERAL_Server {
     static int port_of_server = 1800;
-    static JFrame frame;
+//    static JFrame frame;
     public static void main(String[] args) throws IOException {
+        Combo_General.make_start(true);
 
 
 
-        frame = Combo_General.creatOkno();
-        Combo_General.pane = Combo_General.pane_made(frame);
-        Combo_General.isServer = true;
+//        frame = Combo_General.creatOkno();
+//        Combo_General.pane = Combo_General.pane_made(frame);
+//        Combo_General.isServer = true;
 
 
 
-        ArrayList<MapLocation> maps = new ArrayList<>();
-        MapLocation map = Combo_General.creatMap(-2000,-2000,("Правая нижняя часть карты 2.png"),frame);
-        maps.add(map);
-        Combo_General.maps = maps;
+//        ArrayList<MapLocation> maps = new ArrayList<>();
+//        MapLocation map = Combo_General.creatMap(-2000,-2000,("Правая нижняя часть карты 2.png"),frame);
+//        maps.add(map);
+//        Combo_General.maps = maps;
 
-        Combo_General.list_players = new ArrayList<>();
-        Pers player = Combo_General.player_make(new ImageIcon("pers.png"), 300, 300, "I", -2000, -2000);
-        Combo_General.list_players.add(player);
 
-        frame.setVisible(true);
+//        Combo_General.list_players = new ArrayList<>();
+//        Pers player = Combo_General.player_make(new ImageIcon("pers.png"), 300, 300, "I", -2000, -2000);
+//        Combo_General.list_players.add(player);
 
-        controller controller = new controller(frame, player, maps);
+//        frame.setVisible(true);
+//
+//        controller controller = new controller(frame, player, maps);
 
 
         Thread thread = new Thread(new Runnable() {
